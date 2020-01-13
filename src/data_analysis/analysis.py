180836f8ff7a9faef34a1caeaacff00324462fc3
Jacob Elvin Bryon <jbryon@princeton.edu>
2020-01-13 17:24:20 -0500

    added Doxy documentatioon and appropriate comments

--- src/data_analysis/analysis.py
+++ src/data_analysis/analysis.py
@@ -1,7 +1,9 @@
-"""@package docstring
-Jake Bryon, Dec 9 2019
-writing a basic script to explore making interactive plots
-using a class structure
+"""
+analysis contains the Analysis class which handles 
+all storage and manipulation of input data.
+It stores data in two attributes: data_raw and data_adjusted, with 
+data_raw being the raw input that is never changed and data_adjusted
+starting at the raw input and being able to be changed with class methods.
 """
 import numpy as np
 import numpy.random as npr
@@ -12,36 +14,68 @@ from datetime import date
 import csv
 from scipy import integrate
 
-### defining class for preforming analysis on data
-"""
-Analysis class takes in raw data as input and handles all manipulation of data
-it stores data into two main pieces, data raw, which is never altered, and
-data_adjusted which is manipulated with the other mthods
-"""
 class Analysis:
+    """
+    The Analysis class handles all storage and manipulation of input data.
+    It stores data in two attributes: data_raw and data_adjusted, with 
+    data_raw being the raw input that is never changed and data_adjusted
+    starting at the raw input and being able to be changed with class methods.
 
-    def __init__(self, data_raw):
-        self.data_raw = data_raw
-        self.data_adjusted = np.copy(data_raw)
+    Class Attributes:
+
+    data_raw: N length numpy array containing input data
+
+    data_adjusted: N length array containing data that has undergone all
+    methods that have been appiled
+
+    Example of using code to take derivitive:
+    data = np.linspace(0,10,10)
+    data_analysis = Analysis(data)
+    data_analysis.differentiate()
+
+    This creates a class that stores the data, a linear spaced array, 
+    and takes the derivitive. In this example, data_adjusted would
+    would be the derivitive of the data array and data_raw would
+    still be the same untouched array.
+    """
+    def __init__(self, data):
+        """
+        data: Nx1 numpy array
+
+        To initialize the class object, need to give an input array
+        which will then be stored data to be manipulated for analysis
+        """
+
+        ## Nx1 numpy array composed of input data, never altered
+        self.data_raw = data
+        ## Nx1 numpy array composed of data, changed by each method run
+        self.data_adjusted = np.copy(data)
 
     def reset(self):
         """ resets data_adjusted back to the raw """
         self.data_adjusted = self.data_raw
 
     def differentiate(self):
-        """ calculates derivitive of the data set """
+        """ calculates derivitive of data_adjusted """
         self.data_adjusted = np.gradient(self.data_adjusted)
 
     def integrate(self):
-        """ calculates integral of the data set """
+        """ calculates integral of data_adjusted """
         self.data_adjusted = integrate.cumtrapz(self.data_adjusted)
 
     def trim(self, start, stop):
-        """ trims the data by cutting array at start and stop """
+        """ 
+        start: int
+        stop: int
+
+        trims data_adjusted by cutting array at start and stop elements
+        of the data_adjsuted array
+        """
         self.data_adjusted = self.data_adjusted[start:stop]
 
     def normalize(self):
-        """ normalizes by dividing by largest element in array """
+        """ 
+        normalizes data_adjusted by dividing by largest element in array 
+        """
         self.data_adjusted = self.data_adjusted/max(self.data_adjusted)
-
 
--- src/data_analysis/display.py
+++ src/data_analysis/display.py
@@ -1,12 +1,7 @@
-"""@package docstring
-Jake Bryon, Dec 16 2019
-Class for displaying data taken from laser expirement
-
-
-issues to address:
-figure out how to plot up to 4 plots and add data to each individual
-subplot and plot them independently
-
+"""
+display contains the Display class which takes in an Analysis class as input
+and handles all plotting and interaction with plots. Also contains methods
+to save data to csv file
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,114 +14,120 @@ from analysis import *
 
 class Display:
     """
-    Creating class which will handle all plotting and interactions
+    Display class handles all plotting and interactions
     with manipulating data via buttons and text boxes
-
-    data: 1xNumPlots array containing data sets of Analysis class
-    NumPlots: int; Number of plots to be displayed 
-    Num: int; spcifies which plot will be manipulated when methods are acted
     """
 
     def __init__(self, data):
         """
-        initialize by creating plot and pulling out data from main data 
+        initialize object by pulling out individual analysis structures from
+        data array, create main figure and all subplots with data
         """
+
+        ## data: numpy array of Analysis class objects containing data
         self.data = data
-        self.NumPlots = data.size
+
+        ## NumPlots: int, number of data sets and plots shown
+        self._NumPlots = data.size
+
+        ## Num: int, activate plot/data set that is being manipulated
+        # plots are zero indexed and move from left to right in order
+        # of numbering. Ex: if wanting to manipulate the first data
+        # set entered or the left most plot, use Num = 0
         self.Num = 0
 
-        #### creating subplots given number requested
-        #### for 1-3 plots, just lay out in a row
-        if self.NumPlots == 1:
+        # creating subplots given number requested
+        # for 1-3 plots, just lay out in a row
+        if self._NumPlots == 1:
             self.fig, self.subplots = ( 
                 plt.subplots(1,1, figsize = [14.0, 7.0]) )
-            ### need subplots to be in a list
+            ## subplots: list containing all subplots of the figure
             self.subplots = [self.subplots]
 
-        elif self.NumPlots == 2:
+        elif self._NumPlots == 2:
             self.fig, self.subplots = ( 
                 plt.subplots(1,2, figsize = [14.0, 7.0]) )
 
-        elif self.NumPlots == 3:
+        elif self._NumPlots == 3:
             self.fig, self.subplots = ( 
                 plt.subplots(1,3, figsize = [14.0, 7.0]) )
-        #### when plotting 4, use 2x2 grid
-        elif self.NumPlots == 4:
-            self.fig, self.subplots_temp = ( 
+        # when plotting 4, use 2x2 grid
+        elif self._NumPlots == 4:
+            self.fig, self._subplots_temp = ( 
                 plt.subplots(2,2, figsize = [14.0, 7.0]) )
             # turn format of subplots array to look like others
             self.subplots = np.array([
-                self.subplots_temp[0,0],
-                self.subplots_temp[0,1],
-                self.subplots_temp[1,0],
-                self.subplots_temp[1,1]
+                self._subplots_temp[0,0],
+                self._subplots_temp[0,1],
+                self._subplots_temp[1,0],
+                self._subplots_temp[1,1]
                 ])
         else:
-            #### given error if number of data sets is too large
+            # given error if number of data sets is too large
             print('This number of plots is not supported')
        
-        ######### plot all lines
+        ## lines: array that holds the lines which are displayed on the 
+        # subplots. 
         self.lines = np.array([0,0,0,0], dtype=object)
-        for i in range(0,self.NumPlots):
+        for i in range(0,self._NumPlots):
             self.subplot_data(i)
 
     def place_buttons(self):
         """
-        method to layout all buttons for interacting with plot
+        method to layout all buttons and text boxes for interacting with plots
         """
-        ########## adjust all subplots to make room for buttons
+        # adjust all subplots to make room for buttons
         self.fig.subplots_adjust(left = 0.15, bottom = 0.2, wspace = 0.1)
 
-        ### placing reset button
+        # placing reset button
         axbutton_reset = plt.axes([0.01, 0.90, 0.08, 0.05])
-        self.button_reset = Button(axbutton_reset, 'Reset')
-        self.button_reset.on_clicked(self.reset)
+        self._button_reset = Button(axbutton_reset, 'Reset')
+        self._button_reset.on_clicked(self.reset)
 
-        ### placing differentiation button
+        # placing differentiation button
         axbutton_dif = plt.axes([0.01, 0.60, 0.08, 0.05])
-        self.button_dif = Button(axbutton_dif, 'Differentiate')
-        self.button_dif.on_clicked(self.differentiate)
+        self._button_dif = Button(axbutton_dif, 'Differentiate')
+        self._button_dif.on_clicked(self.differentiate)
 
-        ### placing integration button
+        # placing integration button
         axbutton_int = plt.axes([0.01, 0.50, 0.08, 0.05])
-        self.button_int = Button(axbutton_int, 'Integrate')
-        self.button_int.on_clicked(self.integrate)
+        self._button_int = Button(axbutton_int, 'Integrate')
+        self._button_int.on_clicked(self.integrate)
 
-        ### placing normalization button
+        # placing normalization button
         axbutton_nor = plt.axes([0.01, 0.40, 0.08, 0.05])
-        self.button_nor = Button(axbutton_nor, 'Normalize')
-        self.button_nor.on_clicked(self.normalize)
+        self._button_nor = Button(axbutton_nor, 'Normalize')
+        self._button_nor.on_clicked(self.normalize)
 
-        ### placing down text box to choose plot for manipulation
+        # placing down text box to choose plot for manipulation
         axtext_subnum = plt.axes([0.03, 0.80, 0.06, 0.05])
-        self.text_subnum = TextBox(axtext_subnum, 'Plot:', 
+        self._text_subnum = TextBox(axtext_subnum, 'Plot:', 
             initial = '0')
-        self.text_subnum.on_submit(self.Num_change)
+        self._text_subnum.on_submit(self.Num_change)
 
-        ### text box for trimming data
+        # text box for trimming data
         axtext_trim = plt.axes([0.03, 0.70, 0.06, 0.05])
-        self.text_trim = TextBox(axtext_trim, 'Trim:',
+        self._text_trim = TextBox(axtext_trim, 'Trim:',
             initial = '0,end')
-        self.text_trim.on_submit(self.trim)
+        self._text_trim.on_submit(self.trim)
 
-        ### text box for dividing and finding ratio of data
+        # text box for dividing and finding ratio of data
         axtext_rato = plt.axes([0.03, 0.30, 0.06, 0.05])
-        self.text_rato = TextBox(axtext_rato, 'Ratio:',
+        self._text_rato = TextBox(axtext_rato, 'Ratio:',
             initial = '1')
-        self.text_rato.on_submit(self.ratio)
+        self._text_rato.on_submit(self.ratio)
 
-        ### text box for saving data
+        # text box for saving data
         axtext_save = plt.axes([0.15, 0.10, 0.15, 0.05])
-        self.text_save = TextBox(axtext_save, 'Save Adjusted Data (.csv):',
+        self._text_save = TextBox(axtext_save, 'Save Adjusted Data (.csv):',
             initial = 'DataAdjusted')
-        self.text_save.on_submit(self.saveData)
+        self._text_save.on_submit(self.saveData)
  
 
     def subplot_data(self, plot_num, line_style = '-'):
         """
         function to append data to specified subplot
 
-        lines: array containing the 2D lines for each plot
         plot_num: int; specifices which subplot to plot
         line_style: sets how to plot the data, default is just a normal line
         """
@@ -146,7 +147,7 @@ class Display:
         else:
              print('Data not found for plot')
 
-    ### functions used for buttons
+    # functions used for buttons
     def reset(self, event):
         """
         function to restore adjusted data back to the raw original
@@ -165,7 +166,6 @@ class Display:
         fucntion to differentiate specified data set
         """
 
-        ### computer derivitive
         self.data[self.Num].differentiate()
 
         self.subplots[self.Num].cla()
@@ -177,7 +177,6 @@ class Display:
         fucntion to integrate specified data set
         """
 
-        ### computer derivitive
         self.data[self.Num].integrate()
 
         self.subplots[self.Num].cla()
@@ -189,14 +188,13 @@ class Display:
         fucntion to normalize specified data set
         """
 
-        ### computer derivitive
         self.data[self.Num].normalize()
 
         self.subplots[self.Num].cla()
         self.subplot_data(plot_num = self.Num)
 
 
-    ### functions used for text box entries
+    # functions used for text box entries
     def Num_change(self, text):
         """
         method to change Num which tells the plot to be manipulated
@@ -212,23 +210,23 @@ class Display:
         """
         x_span = text.split(',')
         
-        ### make sure input correct length
+        # make sure input correct length
         if len(x_span) != 2: print('please enter "start,stop"')
 
-        ## pull out starting value
+        # pull out starting value
         x_start = int(x_span[0])
 
-        ### pull out stop value
+        # pull out stop value
         if x_span[1] == 'end':
             x_stop = -1
         else:
             x_stop = int(x_span[1])
 
-        ### trim the data
+        # trim the data
         self.data[self.Num].trim(x_start, x_stop)
-        ### clear data from figure
+        # clear data from figure
         self.subplots[self.Num].cla()
-        ### append subplot with new data 
+        # append subplot with new data 
         self.subplot_data(plot_num = self.Num)
 
 
@@ -251,12 +249,12 @@ class Display:
         method to create a folder with the days date. This folder will be used
         to save specified data into
         """
-        ### finding the date and making a directory with the date
+        # finding the date and making a directory with the date
         today = date.today()
         Today_Date = today.strftime("%b_%d_%Y")
         
         Date_Directory = os.path.join(Today_Date)
-        ### check if the directory exists, if not create it
+        # check if the directory exists, if not create it
         if os.path.exists(Date_Directory):
             print(Date_Directory + ' : exists')
             if os.path.isdir(Date_Directory):
@@ -274,9 +272,9 @@ class Display:
             delimiter = ','
             )
 
-    ### simple plotting method
     def show(self):
         """
         function to display all plots
         """
         plt.show()
+
