ba12e9f6e9c82c244e8c88bbd623dfdb2e8ac540
Jacob Elvin Bryon <jbryon@princeton.edu>
2020-01-14 12:15:51 -0500

    added comments and adjusted Doxygen to special format

--- src/data_analysis/display.py
+++ src/data_analysis/display.py
@@ -1,8 +1,8 @@
-"""
-display contains the Display class which takes in an Analysis class as input
-and handles all plotting and interaction with plots. Also contains methods
-to save data to csv file
-"""
+## 
+# display contains the Display class which takes in an Analysis class as input
+# and handles all plotting and interaction with plots. Also contains methods
+# to save data to csv file
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import TextBox, Button
@@ -12,19 +12,17 @@ import csv
 from analysis import *
 
 
+## Display class handles all plotting and interactions
+# with manipulating data via buttons and text boxes
 class Display:
-    """
-    Display class handles all plotting and interactions
-    with manipulating data via buttons and text boxes
-    """
 
+    ## initialize object by pulling out individual analysis structures from
+    # data array, create main figure and all subplots with data
+    # @param self the object pointer
+    # @param data numpy array containing Analysis classes of data
+    #
     def __init__(self, data):
-        """
-        initialize object by pulling out individual analysis structures from
-        data array, create main figure and all subplots with data
-        """
-
-        ## data: numpy array of Analysis class objects containing data
+        #### data: numpy array of Analysis class objects containing data
         self.data = data
 
         ## NumPlots: int, number of data sets and plots shown
@@ -72,66 +70,70 @@ class Display:
         for i in range(0,self._NumPlots):
             self.subplot_data(i)
 
+
+    ## method to layout all buttons and text boxes for interacting with plots
+    #
     def place_buttons(self):
-        """
-        method to layout all buttons and text boxes for interacting with plots
-        """
+
         # adjust all subplots to make room for buttons
-        self.fig.subplots_adjust(left = 0.15, bottom = 0.2, wspace = 0.1)
+        self.fig.subplots_adjust(left = 0.18, bottom = 0.2, wspace = 0.2)
+        self.fig.suptitle('Alira Laser Data Plotting')
 
         # placing reset button
-        axbutton_reset = plt.axes([0.01, 0.90, 0.08, 0.05])
+        axbutton_reset = plt.axes([0.03, 0.90, 0.08, 0.05])
         self._button_reset = Button(axbutton_reset, 'Reset')
         self._button_reset.on_clicked(self.reset)
 
         # placing differentiation button
-        axbutton_dif = plt.axes([0.01, 0.60, 0.08, 0.05])
+        axbutton_dif = plt.axes([0.03, 0.60, 0.08, 0.05])
         self._button_dif = Button(axbutton_dif, 'Differentiate')
         self._button_dif.on_clicked(self.differentiate)
 
         # placing integration button
-        axbutton_int = plt.axes([0.01, 0.50, 0.08, 0.05])
+        axbutton_int = plt.axes([0.03, 0.50, 0.08, 0.05])
         self._button_int = Button(axbutton_int, 'Integrate')
         self._button_int.on_clicked(self.integrate)
 
         # placing normalization button
-        axbutton_nor = plt.axes([0.01, 0.40, 0.08, 0.05])
+        axbutton_nor = plt.axes([0.03, 0.40, 0.08, 0.05])
         self._button_nor = Button(axbutton_nor, 'Normalize')
         self._button_nor.on_clicked(self.normalize)
 
         # placing down text box to choose plot for manipulation
-        axtext_subnum = plt.axes([0.03, 0.80, 0.06, 0.05])
+        axtext_subnum = plt.axes([0.05, 0.80, 0.06, 0.05])
         self._text_subnum = TextBox(axtext_subnum, 'Plot:', 
             initial = '0')
         self._text_subnum.on_submit(self.Num_change)
 
         # text box for trimming data
-        axtext_trim = plt.axes([0.03, 0.70, 0.06, 0.05])
+        axtext_trim = plt.axes([0.05, 0.70, 0.06, 0.05])
         self._text_trim = TextBox(axtext_trim, 'Trim:',
             initial = '0,end')
         self._text_trim.on_submit(self.trim)
 
         # text box for dividing and finding ratio of data
-        axtext_rato = plt.axes([0.03, 0.30, 0.06, 0.05])
+        axtext_rato = plt.axes([0.05, 0.30, 0.06, 0.05])
         self._text_rato = TextBox(axtext_rato, 'Ratio:',
             initial = '1')
         self._text_rato.on_submit(self.ratio)
 
         # text box for saving data
-        axtext_save = plt.axes([0.15, 0.10, 0.15, 0.05])
+        axtext_save = plt.axes([0.15, 0.08, 0.15, 0.05])
         self._text_save = TextBox(axtext_save, 'Save Adjusted Data (.csv):',
             initial = 'DataAdjusted')
         self._text_save.on_submit(self.saveData)
  
 
+    ## method to append data to specified subplot
+    # This method is used when methods that change the specifed data set
+    # are called in order to replot and update that subplot
+    # 
+    # @param self the object pointer
+    # @param plot_num int; specifices which subplot to plot
+    # @param line_style sets line style, default is just a normal line
+    #
     def subplot_data(self, plot_num, line_style = '-'):
-        """
-        function to append data to specified subplot
-
-        plot_num: int; specifices which subplot to plot
-        line_style: sets how to plot the data, default is just a normal line
-        """
-        
+       
         if plot_num == 0:
             self.lines[0] = self.subplots[0].plot(
                 self.data[0].data_adjusted, line_style)
@@ -147,11 +149,14 @@ class Display:
         else:
              print('Data not found for plot')
 
-    # functions used for buttons
+
+    ## method to restore adjusted data back to the raw original
+    # Calls analysis.reset() on specified data set
+    #
+    # @param self the object pointer
+    # @param event click action on button, activates method
+    #
     def reset(self, event):
-        """
-        function to restore adjusted data back to the raw original
-        """
 
         self.data[self.Num].reset()
 
@@ -161,53 +166,68 @@ class Display:
         self.subplot_data(plot_num = self.Num)
 
 
+    ## method to differentiate specified data set
+    # Calls Analysis.differentiate() on specified data set
+    #
+    # @param self the object pointer
+    # @param event click action on button, activates method
+    #
     def differentiate(self, event):
-        """
-        fucntion to differentiate specified data set
-        """
-
+        
         self.data[self.Num].differentiate()
 
         self.subplots[self.Num].cla()
         self.subplot_data(plot_num = self.Num)
 
 
+    ## method to integrate specified data set
+    # calls Analysis.integrate() on specifed data set
+    #
+    # @param self the object pointer
+    # @param event click action on button, activates method
+    #
     def integrate(self, event):
-        """
-        fucntion to integrate specified data set
-        """
-
         self.data[self.Num].integrate()
 
         self.subplots[self.Num].cla()
         self.subplot_data(plot_num = self.Num)
 
-
+    ## method to normalize specified data set
+    # calls Analysis.normalize() on specifed data set
+    #
+    # @param self the object pointer
+    # @param event click action on button, activates method
+    #
     def normalize(self, event):
-        """
-        fucntion to normalize specified data set
-        """
-
         self.data[self.Num].normalize()
 
         self.subplots[self.Num].cla()
         self.subplot_data(plot_num = self.Num)
 
 
-    # functions used for text box entries
+    ## method to change which data set is being altered, ie change Num
+    #
+    # @param self the object pointer
+    # @param text text input from textbox, must be int;
+    #   specifies which plot will be affected by buttons and methods called,
+    #   changes the Num variable
+    #
     def Num_change(self, text):
-        """
-        method to change Num which tells the plot to be manipulated
-        """
         plot_number = text
         self.Num = int(plot_number)
         print(self.Num)
 
-
+    ## method to specify a range within the data to cut out, so 
+    # anything outside of the range will be taken out of the data_adjusted
+    # Calls Analysis.trim()
+    #
+    # @param self the object pointer
+    # @param text text input from textbox, must be of form 'int1,int2'
+    #   where int1 is the first integer that marks the start of the trim
+    #   int2 marks end, when trim happens only elements found within 
+    #   range of int1 and int2 in the array elements will be left
+    #
     def trim(self, text):
-        """
-        method to specify a range of data to work with, trim off other parts
-        """
         x_span = text.split(',')
         
         # make sure input correct length
@@ -230,10 +250,18 @@ class Display:
         self.subplot_data(plot_num = self.Num)
 
 
+    ## method to take the ratio of current specified data set with another
+    # data set. 
+    # Calls method Analysis.ratio()
+    #
+    # @param self the object pointer
+    # @param text text entered from text box, must be integer; 
+    #   specified the plot the plot that will be element wise divided by the
+    #   current plot, designated by Num. 
+    #   ie: if I am currently working on data in plot 0, if I want to find
+    #   ratio of plot 0 over plot 2, I would enter '2'
+    #
     def ratio(self, text):
-        """
-        method to divide two data sets, find the ratio 
-        """
         ratio_number = int(text)
 
         self.data[self.Num].ratio(self.data[ratio_number].data_adjusted)
@@ -242,11 +270,16 @@ class Display:
         self.subplot_data(plot_num = self.Num)
 
 
+    ## method to save the specifed data set in a csv format
+    # creates a folder with the name of the current date in format:
+    #   Month_Day_Year
+    #
+    # @param self the object pointer
+    # @param text text entered from text box, the text will be the name 
+    #   that the csv will be saved as. ie: if I enter 'filename' it will
+    #   save the specified data set to 'DATE/filename.csv'
+    #
     def saveData(self, text):
-        """
-        method to create a folder with the days date. This folder will be used
-        to save specified data into
-        """
         # finding the date and making a directory with the date
         today = date.today()
         Today_Date = today.strftime("%b_%d_%Y")
@@ -254,12 +287,9 @@ class Display:
         Date_Directory = os.path.join(Today_Date)
         # check if the directory exists, if not create it
         if os.path.exists(Date_Directory):
-            print(Date_Directory + ' : exists')
             if os.path.isdir(Date_Directory):
                 print(Date_Directory + ' : is a directory')
         else:
-            print('no such directory')
-            print('making directory')
             os.mkdir(Date_Directory)
 
         SaveName = text
@@ -270,9 +300,10 @@ class Display:
             delimiter = ','
             )
 
+    ## method to show plots
+    #
+    # @param self the object pointer
+    #
     def show(self):
-        """
-        function to display all plots
-        """
         plt.show()
 
